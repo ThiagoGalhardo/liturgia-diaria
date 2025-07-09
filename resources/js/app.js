24b540c429095a34dfe25ced3c6a64558792237a
Thiago Galhardo <thiago.galhardo@hotmail.com>
2025-07-09 00:11:52 -0300

    add ziggy-js

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,7 +5,7 @@ import { createApp, h } from "vue";
 import { createInertiaApp } from "@inertiajs/inertia-vue3";
 import { InertiaProgress } from "@inertiajs/progress";
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
-import { ZiggyVue } from "../../vendor/tightenco/ziggy/dist/vue.m";
+import { ZiggyVue } from 'ziggy-js';
 
 //Quasar
 import { Quasar, LocalStorage, SessionStorage, Loading } from "quasar";
